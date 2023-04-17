043ac7cbe982de48da613f65738530e40b330d0d
Tibor Kiss <the1bithu@gmail.com>
2023-04-17 18:57:57 +0200

    Hiba

--- app.js
+++ app.js
@@ -24,8 +24,7 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
-  
+  next(createError(404))
 });
 
 // error handler
